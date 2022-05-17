d33e34e9ee94798e45aa92661a294398214fbd2b
Alejandro Delgado Ontana <alejandro@Alejandros-MacBook-Pro.local>
2022-05-17 20:45:47 +0200

    implementing solution

---
+++ test.js
@@ -0,0 +1,18 @@
+/* global test, expect */
+const translate = require('./index.js')
+
+test('it translates "Hello" succesfully', () => {
+  expect(
+    translate(
+      '👇🤜👇👇👇👇👇👇👇👉👆👈🤛👉👇👊👇🤜👇👉👆👆👆👆👆👈🤛👉👆👆👊👆👆👆👆👆👆👆👊👊👆👆👆👊'
+    )
+  ).toBe('Hello')
+})
+
+test('it translates "Hello World!" succesfully', () => {
+  expect(
+    translate(
+      '👉👆👆👆👆👆👆👆👆🤜👇👈👆👆👆👆👆👆👆👆👆👉🤛👈👊👉👉👆👉👇🤜👆🤛👆👆👉👆👆👉👆👆👆🤜👉🤜👇👉👆👆👆👈👈👆👆👆👉🤛👈👈🤛👉👇👇👇👇👇👊👉👇👉👆👆👆👊👊👆👆👆👊👉👇👊👈👈👆🤜👉🤜👆👉👆🤛👉👉🤛👈👇👇👇👇👇👇👇👇👇👇👇👇👇👇👊👉👉👊👆👆👆👊👇👇👇👇👇👇👊👇👇👇👇👇👇👇👇👊👉👆👊👉👆👊'
+    )
+  ).toBe('Hello World!\n')
+})
